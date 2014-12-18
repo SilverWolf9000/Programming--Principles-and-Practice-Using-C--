b9dd755837d9247199cb9957a61aaf3a72aecafb
Jacob Riecke <jwriecke98@gmail.com>
2014-12-17 21:08:12 -0500

    ch2_ex3

---
+++ ch2_ex3.cpp
@@ -0,0 +1,7 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "How to Get to My Room:\n1. Walk forward five steps\n2. Turn right\n3. Walk up three steps\n4. Walk forward eight steps\n5. Turn right\nYou have arrived to my room!";
+    return 0;
+}

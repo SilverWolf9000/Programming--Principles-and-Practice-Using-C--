8027a12a894475689bd7ff2daec1f57a0a2d8421
Jacob Riecke <jwriecke98@gmail.com>
2014-12-17 21:08:17 -0500

    ch2_ex4

---
+++ ch2_ex4.cpp
@@ -0,0 +1,10 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "Recipe for Swedish Doughnuts\n\n";
+    cout << "Ingredients:\n\n2 eggs\n1 cup sugar\n2 cups cold mashed potatoes(mashed with milk and butter)\n3/4 cup buttermilk\n2 tablespoons butter, melted\n1 teaspoon vanilla/almond extract\n4-1/2 cups all-purpose flour\n4 teaspoons baking powder\n1 teaspoon baking soda\n1 teaspoon salt\n2 teaspoons ground nutmeg\n1/8 teaspoon ground ginger\noil for deep-fat frying\nadditional sugar optional\n\n";
+    cout << "Instructions:\n\n1. Take out bowl\n2. Put eggs and sugar in bowl\n3. Beat with whisk in circular motion\n4. Put potatoes, buttermilk, butter, and vanilla in bowl\n5. Combine flour, baking powder, baking soda, salt, nutmeg, and ginger\n6. Add combination to bowl and mix well\n7. Cover bowl\n8. Put in refrigerator for 2 hours\n9. Put flour on wax paper and spread flour\n10. Take out bowl, take out mixture, and place on flour surface\n11. Roll dough to 1/2 inch thick\n12. Cut the roll with flour-covered 2-1/2 inch doughnut cutter\n13. Turn on deep-fryer and heat oil to 375 degrees F\n14. Place doughnuts in fryer two at a time until all golden brown (2 minutes per pair)\n\n";
+    cout << "Congratulations! You made Swedish Doughnuts!\n";
+    return 0;
+}

d479128e0479f0a0e583b2d0e6582b8e54b18489
Jacob Riecke <jwriecke98@gmail.com>
2014-12-17 21:08:07 -0500

    ch2_ex2

---
+++ ch2_ex2.cpp
@@ -0,0 +1,7 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "Directions for finding the bathroom in my household from the front door:\n1: Step forward five steps\n2: Turn right\n3: Climb three stairs\n4: Step forward two steps\n5: Turn left\nYou have arrived at the upstairs bathroom!";
+    return 0;
+}
